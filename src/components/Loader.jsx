7bf56805148718ab4b2b22535c52e2e8d826c253
shakti765477 <shakti765477@gmail.com>
2026-04-06 22:03:14 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,1 +1,46 @@
+import { useEffect, useState } from "react";
 
+export default function Loader({ onFinish }) {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const fullText = "EKAGRA";
+    let i = 0;
+
+    const typing = setInterval(() => {
+      setText(fullText.slice(0, i + 1));
+      i++;
+
+      if (i === fullText.length) {
+        clearInterval(typing);
+
+        setTimeout(() => {
+          onFinish(); // hide loader
+        }, 800);
+      }
+    }, 150);
+
+    return () => clearInterval(typing);
+  }, []);
+
+  return (
+    <div style={styles.loader}>
+      <h1>{text}</h1>
+    </div>
+  );
+}
+
+const styles = {
+  loader: {
+    position: "fixed",
+    width: "100%",
+    height: "100vh",
+    background: "black",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "3rem",
+    zIndex: 9999,
+  },
+};
